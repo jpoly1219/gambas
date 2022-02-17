ce337908ea416a96db8f4b3f28f7565c5baf595b
Jacob Kim <jpoly1219@gmail.com>
2022-02-17 15:01:31 +0900

    using map data type to implement Series data instead

--- types.go
+++ types.go
@@ -4,36 +4,26 @@ import (
 	"fmt"
 )
 
-type Element struct {
-	Value interface{}
-}
-
 type Index struct {
 	Value interface{}
 }
 
 type Series struct {
-	Elements []Element
-	Indexes  []Index
-	Name     string
+	Data map[Index][]interface{}
+	Name string
 }
 
 func (s Series) PrintElements() {
 	if s.Name != "" {
 		fmt.Println(" ", s.Name)
 	}
-	for i, e := range s.Elements {
-		fmt.Println(s.Indexes[i].Value, e.Value)
+	for k, v := range s.Data {
+		fmt.Println(k.Value, v)
 	}
 }
 
-// func main() {
-// 	data := []Element{{"alice"}, {"bob"}, {"charlie"}}
-
-// 	series1, err := NewSeries(data, nil, "")
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-
-// 	series1.PrintElements()
-// }
+type DataFrame struct {
+	SeriesArray []Series
+	IndexArray  []Index
+	Name        string
+}
