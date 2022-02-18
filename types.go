3a62fdec419a8a3463675c87d5d783d188aef09c
Jacob Kim <jpoly1219@gmail.com>
2022-02-18 20:31:00 +0900

    fixed Series.Data's key value to be of type Index instead of interface{}

--- types.go
+++ types.go
@@ -1,25 +1,60 @@
 package gambas
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
-type CustomIndex struct {
-	Value []interface{}
+type Index struct {
+	Value interface{}
 }
 
 type Series struct {
-	Data        map[interface{}]interface{}
-	CustomIndex CustomIndex
-	Name        string
+	Data       map[Index]interface{}
+	IndexArray []Index
+	Name       string
 }
 
 func (s Series) PrintSeries() string {
-	message := fmt.Sprintln("Data:", s.Data, "\nCustomIndex:", s.CustomIndex, "\nName:", s.Name)
+	message := fmt.Sprintln("Data:", s.Data, "\nIndexArray:", s.IndexArray, "\nName:", s.Name)
 	fmt.Println(message)
 	return message
 }
 
+func (s Series) CalcMean() (float64, error) {
+	sum := 0.0
+	for _, v := range s.Data {
+		if reflect.ValueOf(v).Kind() != reflect.Float64 || reflect.ValueOf(v).Kind() != reflect.Int {
+			err := fmt.Errorf("type is not int or float64: %T", v)
+			fmt.Println(err)
+			return 0.0, err
+		}
+		sum += v.(float64)
+	}
+	mean := sum / float64(len(s.Data))
+
+	return mean, nil
+}
+
+func (s Series) Loc(index Index) interface{} {
+	result := s.Data[index]
+
+	return result
+}
+
+func (s Series) LocM(indexArray []Index) []interface{} {
+	resultArray := make([]interface{}, len(indexArray))
+
+	for _, v := range indexArray {
+		result := s.Data[v]
+		resultArray = append(resultArray, result)
+	}
+
+	return resultArray
+}
+
 type DataFrame struct {
-	SeriesArray      []Series
-	CustomIndexArray []CustomIndex
-	Name             string
+	SeriesArray []Series
+	IndexArray  []Index
+	Name        string
 }
