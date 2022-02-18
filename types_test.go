136d59efad68482fcccd2b88fd022872a05d1bf6
Jacob Kim <jpoly1219@gmail.com>
2022-02-18 20:30:52 +0900

    updated test to be more table-driven

--- types_test.go
+++ types_test.go
@@ -2,16 +2,33 @@ package gambas
 
 import "testing"
 
+var seriesArray = []Series{
+	{
+		Data:       map[Index]interface{}{{0}: "alice", {1}: "bob", {2}: "charlie"},
+		IndexArray: []Index{{0}, {1}, {2}},
+		Name:       "People",
+	},
+	{
+		Data:       map[Index]interface{}{{"a"}: "apple", {"b"}: "banana", {"c"}: "cherry"},
+		IndexArray: []Index{{"a"}, {"b"}, {"c"}},
+		Name:       "Fruit",
+	},
+}
+
 func TestPrintSeries(t *testing.T) {
-	s := Series{
-		Data: map[interface{}]interface{}{0: "alice", 1: "bob", 2: "charlie"},
-		Name: "People",
+	expectedArray := []string{
+		"Data: map[{0}:alice {1}:bob {2}:charlie] \nIndexArray: [{0} {1} {2}] \nName: People\n",
+		"Data: map[{a}:apple {b}:banana {c}:cherry] \nIndexArray: [{a} {b} {c}] \nName: Fruit\n",
 	}
 
-	expected := "Data: map[0:alice 1:bob 2:charlie] \nCustomIndex: {[]} \nName: People\n"
-	output := s.PrintSeries()
-
-	if output != expected {
-		t.Fatalf("\nexpected:\n%s,\ngot:\n%s\n", expected, output)
+	for i, test := range seriesArray {
+		output := test.PrintSeries()
+		if output != expectedArray[i] {
+			t.Fatalf("expected: %s, got: %s", expectedArray[i], output)
+		}
 	}
+}
+
+func TestLoc(t *testing.T) {
+
 }
