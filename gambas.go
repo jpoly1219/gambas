eb888b4efe60f459b078388c9d90623458448e26
Jacob Kim <jpoly1219@gmail.com>
2022-02-18 20:46:57 +0900

    fixed a bug where s.IndexArray will reset for every iteration

--- gambas.go
+++ gambas.go
@@ -5,11 +5,11 @@ import "fmt"
 func NewSeries(data []interface{}, indexes []Index, name string) Series {
 	var s Series
 	s.Data = make(map[Index]interface{})
+	s.IndexArray = make([]Index, len(data))
 
 	for i, v := range data {
 		if indexes == nil {
-			s.IndexArray = make([]Index, len(data))
-			s.IndexArray = append(s.IndexArray, Index{i})
+			s.IndexArray[i] = Index{i}
 			s.Data[Index{i}] = v
 		} else {
 			s.IndexArray = indexes
