475b661eb07f706b75f429e87e9efdfb5dbfaa7e
Jacob Kim <jpoly1219@gmail.com>
2022-02-19 12:11:32 +0900

    created tests for Series.Loc() and Series.LocM()

--- types_test.go
+++ types_test.go
@@ -1,21 +1,24 @@
 package gambas
 
-import "testing"
+import (
+	"testing"
 
-var seriesArray = []Series{
-	{
-		Data:       map[Index]interface{}{{0}: "alice", {1}: "bob", {2}: "charlie"},
-		IndexArray: []Index{{0}, {1}, {2}},
-		Name:       "People",
-	},
-	{
-		Data:       map[Index]interface{}{{"a"}: "apple", {"b"}: "banana", {"c"}: "cherry"},
-		IndexArray: []Index{{"a"}, {"b"}, {"c"}},
-		Name:       "Fruit",
-	},
-}
+	"github.com/google/go-cmp/cmp"
+)
 
 func TestPrintSeries(t *testing.T) {
+	seriesArray := []Series{
+		{
+			Data:       map[Index]interface{}{{0}: "alice", {1}: "bob", {2}: "charlie"},
+			IndexArray: []Index{{0}, {1}, {2}},
+			Name:       "People",
+		},
+		{
+			Data:       map[Index]interface{}{{"a"}: "apple", {"b"}: "banana", {"c"}: "cherry"},
+			IndexArray: []Index{{"a"}, {"b"}, {"c"}},
+			Name:       "Fruit",
+		},
+	}
 	expectedArray := []string{
 		"Data: map[{0}:alice {1}:bob {2}:charlie] \nIndexArray: [{0} {1} {2}] \nName: People\n",
 		"Data: map[{a}:apple {b}:banana {c}:cherry] \nIndexArray: [{a} {b} {c}] \nName: Fruit\n",
@@ -30,5 +33,71 @@ func TestPrintSeries(t *testing.T) {
 }
 
 func TestLoc(t *testing.T) {
+	type locTest struct {
+		arg1     Series
+		arg2     Index
+		expected interface{}
+	}
+	locTests := []locTest{
+		{
+			Series{
+				map[Index]interface{}{{0}: "alice", {1}: "bob", {2}: "charlie"},
+				[]Index{{0}, {1}, {2}},
+				"People",
+			},
+			Index{0},
+			"alice",
+		},
+		{
+			Series{
+				map[Index]interface{}{{"a"}: "apple", {"b"}: "banana", {"c"}: "cherry"},
+				[]Index{{"a"}, {"b"}, {"c"}},
+				"Fruit",
+			},
+			Index{"b"},
+			"banana",
+		},
+	}
 
+	for _, test := range locTests {
+		output := test.arg1.Loc(test.arg2)
+		if output != test.expected {
+			t.Fatalf("expected %v, got %v", test.expected, output)
+		}
+	}
+}
+
+func TestLocM(t *testing.T) {
+	type locMTest struct {
+		arg1     Series
+		arg2     []Index
+		expected []interface{}
+	}
+	locMTests := []locMTest{
+		{
+			Series{
+				map[Index]interface{}{{0}: "alice", {1}: "bob", {2}: "charlie"},
+				[]Index{{0}, {1}, {2}},
+				"People",
+			},
+			[]Index{{0}, {1}},
+			[]interface{}{"alice", "bob"},
+		},
+		{
+			Series{
+				map[Index]interface{}{{"a"}: "apple", {"b"}: "banana", {"c"}: "cherry"},
+				[]Index{{"a"}, {"b"}, {"c"}},
+				"Fruit",
+			},
+			[]Index{{"b"}, {"c"}},
+			[]interface{}{"banana", "cherry"},
+		},
+	}
+
+	for _, test := range locMTests {
+		output := test.arg1.LocM(test.arg2)
+		if !cmp.Equal(output, test.expected) {
+			t.Fatalf("expected %v, got %v", test.expected, output)
+		}
+	}
 }
