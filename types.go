d14f3ea5e60cca8f15b91fc80ffef318f1dc53ea
Jacob Kim <jpoly1219@gmail.com>
2022-02-21 15:45:41 +0900

    finalized data type for Index and Series

--- types.go
+++ types.go
@@ -6,17 +6,17 @@ import (
 )
 
 type Index struct {
-	value interface{}
+	data []interface{}
 }
 
 type Series struct {
-	data       map[Index]interface{}
-	indexArray []Index
-	name       string
+	data  map[interface{}]interface{}
+	index Index
+	name  string
 }
 
 func (s Series) PrintSeries() string {
-	message := fmt.Sprintln("data:", s.data, "\nindexArray:", s.indexArray, "\nname:", s.name)
+	message := fmt.Sprintln("data:", s.data, "\nindexArray:", s.index, "\nname:", s.name)
 	fmt.Println(message)
 	return message
 }
@@ -36,13 +36,15 @@ func (s Series) CalcMean() (float64, error) {
 	return mean, nil
 }
 
-func (s Series) At(index Index) interface{} {
+// At() returns the element at a given index.
+func (s Series) At(index interface{}) interface{} {
 	result := s.data[index]
 
 	return result
 }
 
-func (s Series) AtM(indexArray []Index) []interface{} {
+// AtM() returns an array of elements at given indexes.
+func (s Series) AtM(indexArray []interface{}) []interface{} {
 	resultArray := make([]interface{}, len(indexArray))
 
 	for i, v := range indexArray {
@@ -53,11 +55,12 @@ func (s Series) AtM(indexArray []Index) []interface{} {
 	return resultArray
 }
 
+// AtR() returns an array of elements at a given index range.
 func (s Series) AtR(min, max int) []interface{} {
 	resultArray := make([]interface{}, 0)
 
 	for i := min; i <= max; i++ {
-		key := s.indexArray[i]
+		key := s.index.data[i]
 		result := s.data[key]
 		resultArray = append(resultArray, result)
 	}
@@ -66,7 +69,8 @@ func (s Series) AtR(min, max int) []interface{} {
 }
 
 type DataFrame struct {
-	SeriesArray []Series
-	IndexArray  []Index
-	Name        string
+	series  map[interface{}]Series
+	index   Index
+	columns Index
+	name    string
 }
