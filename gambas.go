a7b984045da7cc2b3cded3c7fd86938bbd8501f2
Jacob Kim <jpoly1219@gmail.com>
2022-02-21 15:46:01 +0900

    updated data types for Index and Series

--- gambas.go
+++ gambas.go
@@ -2,17 +2,17 @@ package gambas
 
 import "fmt"
 
-func NewSeries(data []interface{}, indexes []Index, name string) Series {
+func NewSeries(data []interface{}, indexes []interface{}, name string) Series {
 	var s Series
-	s.data = make(map[Index]interface{})
-	s.indexArray = make([]Index, len(data))
+	s.data = make(map[interface{}]interface{})
+	s.index.data = make([]interface{}, len(indexes))
 
 	for i, v := range data {
 		if indexes == nil {
-			s.indexArray[i] = Index{i}
-			s.data[Index{i}] = v
+			s.index.data = append(s.index.data, i)
+			s.data[i] = v
 		} else {
-			s.indexArray = indexes
+			s.index.data = indexes
 			s.data[indexes[i]] = v
 		}
 	}
