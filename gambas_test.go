f8f76e262cde665c0b5c3a40192b7af5dcd1e3c2
Jacob Kim <jpoly1219@gmail.com>
2022-02-25 23:04:13 +0900

    created test for NewDataFrame

--- gambas_test.go
+++ gambas_test.go
@@ -44,3 +44,48 @@ func TestNewSeries(t *testing.T) {
 		}
 	}
 }
+
+func TestNewDataFrame(t *testing.T) {
+	type newDataFrameTest struct {
+		arg1     [][]interface{}
+		arg2     []interface{}
+		arg3     []interface{}
+		expected DataFrame
+	}
+
+	newDataFrameTests := []newDataFrameTest{
+		{
+			[][]interface{}{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
+			[]interface{}{0, 1, 2},
+			[]interface{}{"group a", "group b", "group c"},
+			DataFrame{
+				map[interface{}]Series{
+					"group a": {
+						map[interface{}]interface{}{0: 1, 1: 2, 2: 3},
+						Index{[]interface{}{0, 1, 2}},
+						"group a",
+					},
+					"group b": {
+						map[interface{}]interface{}{0: 4, 1: 5, 2: 6},
+						Index{[]interface{}{0, 1, 2}},
+						"group b",
+					},
+					"group c": {
+						map[interface{}]interface{}{0: 7, 1: 8, 2: 9},
+						Index{[]interface{}{0, 1, 2}},
+						"group c",
+					},
+				},
+				Index{[]interface{}{0, 1, 2}},
+				Index{[]interface{}{"group a", "group b", "group c"}},
+			},
+		},
+	}
+
+	for _, test := range newDataFrameTests {
+		output := NewDataFrame(test.arg1, test.arg2, test.arg3)
+		if !cmp.Equal(output, test.expected, cmp.AllowUnexported(output, output.index)) {
+			t.Fatalf("expected %v, got %v", test.expected, output)
+		}
+	}
+}
