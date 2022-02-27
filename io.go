1d1db519e799793b8d7ef6aaf37e84c0ed130d60
Jacob Kim <jpoly1219@gmail.com>
2022-02-27 22:47:24 +0900

    created ReadCsv() to read csv files and create a new DataFrame object

---
+++ io.go
@@ -0,0 +1,52 @@
+package gambas
+
+import (
+	"encoding/csv"
+	"io"
+	"log"
+	"os"
+)
+
+func ReadCsv(pathToFile string, dtype []string) DataFrame {
+	// read line by line
+	f, err := os.Open(pathToFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	csvr := csv.NewReader(f)
+
+	rowNum := 0
+	columnArray := make([]interface{}, 0)
+	data2DArray := make([][]interface{}, 0)
+	for {
+		row, err := csvr.Read()
+		if err != nil {
+			if err == io.EOF {
+				err = nil
+			}
+			log.Fatal(err)
+		}
+		// first line is column name
+		if rowNum == 0 {
+			// add to columnArray
+			for _, v := range row {
+				columnArray = append(columnArray, v)
+			}
+			rowNum++
+			continue
+		}
+		// second line onwards is the actual data
+		dataArray := make([]interface{}, len(row))
+		for _, v := range row {
+			// add to data2DArray
+			dataArray = append(dataArray, v)
+		}
+		data2DArray = append(data2DArray, dataArray)
+		rowNum++
+	}
+	// create new DataFrame object and return it
+	df := NewDataFrame(data2DArray, []interface{}{}, columnArray)
+	return df
+	// each data should be checked to see what type it is
+}
