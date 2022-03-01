004a00ec314cb446df46502cfb69fb90e605ca57
Jacob Kim <jpoly1219@gmail.com>
2022-03-01 21:36:39 +0900

    updated tests to use new data types

--- types_test.go
+++ types_test.go
@@ -9,19 +9,19 @@ import (
 func TestPrintSeries(t *testing.T) {
 	seriesArray := []Series{
 		{
-			map[interface{}]interface{}{0: "alice", 1: "bob", 2: "charlie"},
+			[]interface{}{"alice", "bob", "charlie"},
 			Index{[]interface{}{0, 1, 2}},
 			"People",
 		},
 		{
-			map[interface{}]interface{}{"a": "apple", "b": "banana", "c": "cherry"},
+			[]interface{}{"apple", "banana", "cherry"},
 			Index{[]interface{}{"a", "b", "c"}},
 			"Fruit",
 		},
 	}
 	expectedArray := []string{
-		"data: map[0:alice 1:bob 2:charlie] \nindexArray: {[0 1 2]} \nname: People\n",
-		"data: map[a:apple b:banana c:cherry] \nindexArray: {[a b c]} \nname: Fruit\n",
+		"data: [alice bob charlie] \nindexArray: {[0 1 2]} \nname: People\n",
+		"data: [apple banana cherry] \nindexArray: {[a b c]} \nname: Fruit\n",
 	}
 
 	for i, test := range seriesArray {
@@ -41,7 +41,7 @@ func TestAt(t *testing.T) {
 	atTests := []atTest{
 		{
 			Series{
-				map[interface{}]interface{}{0: "alice", 1: "bob", 2: "charlie"},
+				[]interface{}{"alice", "bob", "charlie"},
 				Index{[]interface{}{0, 1, 2}},
 				"People",
 			},
@@ -50,7 +50,7 @@ func TestAt(t *testing.T) {
 		},
 		{
 			Series{
-				map[interface{}]interface{}{"a": "apple", "b": "banana", "c": "cherry"},
+				[]interface{}{"apple", "banana", "cherry"},
 				Index{[]interface{}{"a", "b", "c"}},
 				"Fruit",
 			},
@@ -60,9 +60,9 @@ func TestAt(t *testing.T) {
 	}
 
 	for _, test := range atTests {
-		output := test.arg1.At(test.arg2)
-		if output != test.expected {
-			t.Fatalf("expected %v, got %v", test.expected, output)
+		output, err := test.arg1.At(test.arg2)
+		if output != test.expected || err != nil {
+			t.Fatalf("expected %v, got %v, error %v", test.expected, output, err)
 		}
 	}
 }
@@ -76,7 +76,7 @@ func TestAtM(t *testing.T) {
 	atMTests := []atMTest{
 		{
 			Series{
-				map[interface{}]interface{}{0: "alice", 1: "bob", 2: "charlie"},
+				[]interface{}{"alice", "bob", "charlie"},
 				Index{[]interface{}{0, 1, 2}},
 				"People",
 			},
@@ -85,7 +85,7 @@ func TestAtM(t *testing.T) {
 		},
 		{
 			Series{
-				map[interface{}]interface{}{"a": "apple", "b": "banana", "c": "cherry"},
+				[]interface{}{"apple", "banana", "cherry"},
 				Index{[]interface{}{"a", "b", "c"}},
 				"Fruit",
 			},
@@ -95,9 +95,9 @@ func TestAtM(t *testing.T) {
 	}
 
 	for _, test := range atMTests {
-		output := test.arg1.AtM(test.arg2)
-		if !cmp.Equal(output, test.expected) {
-			t.Fatalf("expected %v, got %v", test.expected, output)
+		output, err := test.arg1.AtM(test.arg2)
+		if !cmp.Equal(output, test.expected) || err != nil {
+			t.Fatalf("expected %v, got %v, error %v", test.expected, output, err)
 		}
 	}
 }
@@ -112,30 +112,30 @@ func TestAtR(t *testing.T) {
 	atRTests := []atRTest{
 		{
 			Series{
-				map[interface{}]interface{}{0: "alice", 1: "bob", 2: "charlie"},
+				[]interface{}{"alice", "bob", "charlie"},
 				Index{[]interface{}{0, 1, 2}},
 				"People",
 			},
 			0,
 			2,
-			[]interface{}{"alice", "bob", "charlie"},
+			[]interface{}{"alice", "bob"},
 		},
 		{
 			Series{
-				map[interface{}]interface{}{"a": "apple", "b": "banana", "c": "cherry"},
+				[]interface{}{"apple", "banana", "cherry"},
 				Index{[]interface{}{"a", "b", "c"}},
 				"Fruit",
 			},
 			0,
 			1,
-			[]interface{}{"apple", "banana"},
+			[]interface{}{"apple"},
 		},
 	}
 
 	for _, test := range atRTests {
-		output := test.arg1.AtR(test.arg2, test.arg3)
-		if !cmp.Equal(output, test.expected) {
-			t.Fatalf("expected %v, got %v", test.expected, output)
+		output, err := test.arg1.AtR(test.arg2, test.arg3)
+		if !cmp.Equal(output, test.expected) || err != nil {
+			t.Fatalf("expected %v, got %v, error %v", test.expected, output, err)
 		}
 	}
 }
