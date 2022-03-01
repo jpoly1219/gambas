c5c9bfc22bf9fd66174c5e8fa3af67d66417430f
Jacob Kim <jpoly1219@gmail.com>
2022-03-01 21:36:48 +0900

    updated tests to use new data types

--- gambas_test.go
+++ gambas_test.go
@@ -29,7 +29,7 @@ func TestCreateRangeIndex(t *testing.T) {
 
 	for _, test := range createRangeIndexTests {
 		output := CreateRangeIndex(test.arg1)
-		if !cmp.Equal(output, test.expected) {
+		if !cmp.Equal(output, test.expected, cmp.AllowUnexported(output)) {
 			t.Fatalf("expected %v, got %v", test.expected, output)
 		}
 	}
@@ -38,17 +38,17 @@ func TestCreateRangeIndex(t *testing.T) {
 func TestNewSeries(t *testing.T) {
 	type newSeriesTest struct {
 		arg1     []interface{}
-		arg2     []interface{}
+		arg2     Index
 		arg3     string
-		expected Series
+		expected *Series
 	}
 
 	newSeriesTests := []newSeriesTest{
 		{
 			[]interface{}{"alice", "bob", "charlie"},
-			nil,
+			CreateRangeIndex(3),
 			"People",
-			Series{
+			&Series{
 				[]interface{}{"alice", "bob", "charlie"},
 				Index{[]interface{}{0, 1, 2}},
 				"People",
@@ -56,9 +56,9 @@ func TestNewSeries(t *testing.T) {
 		},
 		{
 			[]interface{}{"apple", "banana", "cherry"},
-			[]interface{}{"a", "b", "c"},
+			Index{[]interface{}{"a", "b", "c"}},
 			"Fruit",
-			Series{
+			&Series{
 				[]interface{}{"apple", "banana", "cherry"},
 				Index{[]interface{}{"a", "b", "c"}},
 				"Fruit",
@@ -68,8 +68,8 @@ func TestNewSeries(t *testing.T) {
 
 	for _, test := range newSeriesTests {
 		output, err := NewSeries(test.arg1, test.arg2, test.arg3)
-		if !cmp.Equal(output, test.expected, cmp.AllowUnexported(output, output.index)) {
-			t.Fatalf("expected %v, got %v", test.expected, output)
+		if !cmp.Equal(*output, *test.expected, cmp.AllowUnexported(*output, output.index)) || err != nil {
+			t.Fatalf("expected %v, got %v, error %v", test.expected, output, err)
 		}
 	}
 }
@@ -77,30 +77,30 @@ func TestNewSeries(t *testing.T) {
 func TestNewDataFrame(t *testing.T) {
 	type newDataFrameTest struct {
 		arg1     [][]interface{}
-		arg2     []interface{}
+		arg2     Index
 		arg3     []interface{}
-		expected DataFrame
+		expected *DataFrame
 	}
 
 	newDataFrameTests := []newDataFrameTest{
 		{
 			[][]interface{}{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
-			[]interface{}{0, 1, 2},
+			CreateRangeIndex(3),
 			[]interface{}{"group a", "group b", "group c"},
-			DataFrame{
-				map[interface{}]Series{
-					"group a": {
-						map[interface{}]interface{}{0: 1, 1: 2, 2: 3},
+			&DataFrame{
+				[]Series{
+					{
+						[]interface{}{1, 2, 3},
 						Index{[]interface{}{0, 1, 2}},
 						"group a",
 					},
-					"group b": {
-						map[interface{}]interface{}{0: 4, 1: 5, 2: 6},
+					{
+						[]interface{}{4, 5, 6},
 						Index{[]interface{}{0, 1, 2}},
 						"group b",
 					},
-					"group c": {
-						map[interface{}]interface{}{0: 7, 1: 8, 2: 9},
+					{
+						[]interface{}{7, 8, 9},
 						Index{[]interface{}{0, 1, 2}},
 						"group c",
 					},
@@ -112,9 +112,9 @@ func TestNewDataFrame(t *testing.T) {
 	}
 
 	for _, test := range newDataFrameTests {
-		output := NewDataFrame(test.arg1, test.arg2, test.arg3)
-		if !cmp.Equal(output, test.expected, cmp.AllowUnexported(output, output.series[0], output.index)) {
-			t.Fatalf("expected %v, got %v", test.expected, output)
+		output, err := NewDataFrame(test.arg1, test.arg2, test.arg3)
+		if !cmp.Equal(*output, *test.expected, cmp.AllowUnexported(*output, output.index, output.series[0])) || err != nil {
+			t.Fatalf("expected %v, got %v, error %v", test.expected, output, err)
 		}
 	}
 }
