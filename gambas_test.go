9c34f85dea62bd73537d2a2c54befa41235eeb70
Jacob Kim <jpoly1219@gmail.com>
2022-03-19 15:15:25 +0900

    made TestI2f more robust

--- gambas_test.go
+++ gambas_test.go
@@ -1,6 +1,7 @@
 package gambas
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -148,6 +149,30 @@ func TestCheckTypeIntegrity(t *testing.T) {
 	for _, test := range checkTypeIntegrityTests {
 		output, err := checkTypeIntegrity(test.arg1)
 		if !cmp.Equal(output, test.expected) || (output != test.expected && err == nil) {
+			t.Fatalf("expected %v, got %v, error %v", test.expected, output, err)
+		}
+	}
+}
+
+func TestI2f(t *testing.T) {
+	type i2fTest struct {
+		arg1          interface{}
+		expected      float64
+		expectedError error
+	}
+	i2fTests := []i2fTest{
+		{1, 1.0, nil},
+		{-5, -5.0, nil},
+		{"45", 0.0, fmt.Errorf("%v is not a number", "45")},
+		{2.0, 2.0, nil},
+		{-5.6, -5.6, nil},
+		{0.0, 0.0, nil},
+		{0.000, 0.0, nil},
+	}
+
+	for _, test := range i2fTests {
+		output, err := i2f(test.arg1)
+		if !cmp.Equal(output, test.expected) || !cmp.Equal(fmt.Sprint(err), fmt.Sprint(test.expectedError)) {
 			t.Fatalf("expected %v, got %v, error %v", test.expected, output, err)
 		}
 	}
