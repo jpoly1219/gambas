e4880bcfb60983e3e840bd482fcd73f165eccb19
Jacob Kim <jpoly1219@gmail.com>
2022-03-24 09:49:44 +0900

    moved checkTypeIntegrity and i2f to utils.go

--- gambas_test.go
+++ gambas_test.go
@@ -1,7 +1,6 @@
 package gambas
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -118,66 +117,6 @@ func TestNewDataFrame(t *testing.T) {
 	for _, test := range newDataFrameTests {
 		output, err := NewDataFrame(test.arg1, test.arg2, test.arg3)
 		if !cmp.Equal(*output, *test.expected, cmp.AllowUnexported(*output, output.index[0], output.series[0])) || err != nil {
-			t.Fatalf("expected %v, got %v, error %v", test.expected, output, err)
-		}
-	}
-}
-
-func TestCheckTypeIntegrity(t *testing.T) {
-	type checkTypeIntegrityTest struct {
-		arg1     []interface{}
-		expected bool
-	}
-	checkTypeIntegrityTests := []checkTypeIntegrityTest{
-		{
-			[]interface{}{0, 1, 2, 3, 4, 5, 6},
-			true,
-		},
-		{
-			[]interface{}{3.4, 5.6, 2.4, 6.5, 7},
-			true,
-		},
-		{
-			[]interface{}{1, 2, "3", "4", 5},
-			false,
-		},
-		{
-			[]interface{}{"a", "b", "c"},
-			true,
-		},
-		{
-			[]interface{}{true, false, true},
-			false,
-		},
-	}
-
-	for _, test := range checkTypeIntegrityTests {
-		output, err := checkTypeIntegrity(test.arg1)
-		if !cmp.Equal(output, test.expected) || (output != test.expected && err == nil) {
-			t.Fatalf("expected %v, got %v, error %v", test.expected, output, err)
-		}
-	}
-}
-
-func TestI2f(t *testing.T) {
-	type i2fTest struct {
-		arg1          interface{}
-		expected      float64
-		expectedError error
-	}
-	i2fTests := []i2fTest{
-		{1, 1.0, nil},
-		{-5, -5.0, nil},
-		{"45", 0.0, fmt.Errorf("%v is not a number", "45")},
-		{2.0, 2.0, nil},
-		{-5.6, -5.6, nil},
-		{0.0, 0.0, nil},
-		{0.000, 0.0, nil},
-	}
-
-	for _, test := range i2fTests {
-		output, err := i2f(test.arg1)
-		if !cmp.Equal(output, test.expected) || !cmp.Equal(fmt.Sprint(err), fmt.Sprint(test.expectedError)) {
 			t.Fatalf("expected %v, got %v, error %v", test.expected, output, err)
 		}
 	}
