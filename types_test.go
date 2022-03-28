0d66779eb0f3f0010e1273b9f223abcd45604758
Jacob Kim <jpoly1219@gmail.com>
2022-03-28 10:48:32 +0900

    created tests for Min()

--- types_test.go
+++ types_test.go
@@ -1773,7 +1773,80 @@ func TestStd(t *testing.T) {
 }
 
 func TestMin(t *testing.T) {
+	type minTest struct {
+		arg1          Series
+		expected      float64
+		expectedError error
+	}
+	minTests := []minTest{
+		{
+			Series{
+				[]interface{}{"Avery", "Bradley", "Candice", "Diana"},
+				Index{[]interface{}{0, 1, 2, 3}},
+				"Name",
+			},
+			math.NaN(),
+			fmt.Errorf("data is not a float64: %v", "Avery"),
+		},
+		{
+			Series{
+				[]interface{}{30.0, 23.0, 19.0},
+				Index{[]interface{}{0, 1, 2}},
+				"Age",
+			},
+			19.0,
+			nil,
+		},
+		{
+			Series{
+				[]interface{}{},
+				Index{[]interface{}{}},
+				"Empty",
+			},
+			math.NaN(),
+			fmt.Errorf("no elements in this column"),
+		},
+		{
+			Series{
+				[]interface{}{164.3, 182.5, 173.0, 178.7},
+				Index{[]interface{}{0, 1, 2, 3}},
+				"Height",
+			},
+			164.3,
+			nil,
+		},
+		{
+			Series{
+				[]interface{}{164.3, 182.5, math.NaN(), 178.7},
+				Index{[]interface{}{0, 1, 2, 3}},
+				"Height",
+			},
+			164.3,
+			nil,
+		},
+		{
+			Series{
+				[]interface{}{164.3, math.NaN(), 178.7},
+				Index{[]interface{}{0, 1, 2}},
+				"Height",
+			},
+			164.3,
+			nil,
+		},
+	}
 
+	for _, test := range minTests {
+		output, err := test.arg1.Min()
+		if !cmp.Equal(output, test.expected, cmp.AllowUnexported(Series{}, Series{}.index)) || (fmt.Sprint(err) != fmt.Sprint(test.expectedError)) {
+			if fmt.Sprint(output) == "NaN" {
+				if !cmp.Equal(fmt.Sprint(output), fmt.Sprint(test.expected)) {
+					t.Fatalf("expected %v, got %v, err %v", test.expected, output, err)
+				}
+			} else {
+				t.Fatalf("expected %v, got %v, err %v", test.expected, output, err)
+			}
+		}
+	}
 }
 
 func TestMax(t *testing.T) {
