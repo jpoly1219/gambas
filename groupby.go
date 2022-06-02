f1ff7459fa81b56c5ed6c94c8bf56d9e1bf2bdb3
Jacob Kim <jpoly1219@gmail.com>
2022-06-02 10:29:35 +0900

    changed GroupBy type attributes

--- groupby.go
+++ groupby.go
@@ -1,14 +1,23 @@
 package gambas
 
 type GroupBy struct {
-	index   []interface{}
-	columns []string
-	dataMap map[string][]interface{}
+	DataFrame *DataFrame
+	colIndMap map[string][]interface{}
+	colTuples [][]interface{}
 }
 
 // Agg aggregates data in the GroupBy object using the given aggFunc.
-func (gb *GroupBy) Agg(columns []string, aggFunc StatsFunc) (*DataFrame, error) {
-	statsResultMap := make(map[string]StatsResult, 0)
+func (gb *GroupBy) Agg(targetCol []string, aggFunc StatsFunc) (*DataFrame, error) {
+	valueSlice := make([]interface{}, 0)
+
+	for i, idx := gb.index {
+		for j, col := range gb.columns {
+
+		}
+	}
+
+
+
 	for k, v := range gb.dataMap {
 		result := aggFunc(v)
 		statsResultMap[k] = result
