de4311954da6c33cad9750c850347c8f5d5fbde2
Jacob Kim <jpoly1219@gmail.com>
2022-06-04 00:00:27 +0900

    fixing issue with Agg

--- groupby.go
+++ groupby.go
@@ -1,26 +1,52 @@
 package gambas
 
+import "fmt"
+
 type GroupBy struct {
-	DataFrame *DataFrame
-	colIndMap map[string][]interface{}
-	colTuples [][]interface{}
+	dataFrame       *DataFrame
+	colIndMap       map[string][]interface{}
+	colTuples       [][]interface{}
+	colTuplesLabels []string
 }
 
 // Agg aggregates data in the GroupBy object using the given aggFunc.
 func (gb *GroupBy) Agg(targetCol []string, aggFunc StatsFunc) (*DataFrame, error) {
-	valueSlice := make([]interface{}, 0)
+	filtered, err := gb.dataFrame.LocCols(targetCol...)
+	if err != nil {
+		return nil, err
+	}
 
-	for i, idx := gb.index {
-		for j, col := range gb.columns {
+	newDfData := make([][]interface{}, len(gb.colTuples[0])+len(targetCol))
+	for i, ser := range filtered.series {
+		for j, colTuple := range gb.colTuples {
+			colTupleIndex := Index{j, colTuple}
+			key, err := colTupleIndex.hashKeyValueOnly()
+			if err != nil {
+				return nil, err
+			}
 
+			indexForData := gb.colIndMap[*key]
+			ifd2d := make([][]interface{}, 0)
+			for _, ifd := range indexForData {
+				ifd2d = append(ifd2d, []interface{}{ifd})
+			}
+			data, err := ser.LocItems(ifd2d...)
+			if err != nil {
+				return nil, err
+			}
+			result := aggFunc(data)
+
+			for k := range colTuple {
+				newDfData[i] = append(newDfData[i], colTuple[i])
+			}
 		}
 	}
 
+	fmt.Println(colTupleToResult)
 
-
-	for k, v := range gb.dataMap {
-		result := aggFunc(v)
-		statsResultMap[k] = result
+	newDf, err := NewDataFrame(colTupleToResult, gb.dataFrame.columns, gb.dataFrame.index.names)
+	if err != nil {
+		return nil, err
 	}
-	
+	return newDf, nil
 }
