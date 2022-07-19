51782bec1cfd7d3606fabca23c018b615dba6874
Jacob Kim <jpoly1219@gmail.com>
2022-07-19 10:49:20 +0900

    created TestPlotN

--- plot_test.go
+++ plot_test.go
@@ -25,3 +25,45 @@ func TestPlot(t *testing.T) {
 		}
 	}
 }
+
+func TestPlotN(t *testing.T) {
+	type plotTest struct {
+		arg1 []PlotData
+		arg2 []GnuplotOpt
+	}
+	plotTests := []plotTest{
+		{
+			[]PlotData{
+				{
+					func() DataFrame {
+						newDf, err := ReadCsv("./testfiles/neo_v2.csv", []string{"id"})
+						if err != nil {
+							t.Error(err)
+						}
+						return newDf
+					}(),
+					[]string{"est_diameter_min", "relative_velocity"},
+					[]GnuplotOpt{Using("0:1 lc 0")},
+				},
+				{
+					func() DataFrame {
+						newDf, err := ReadCsv("./testfiles/neo_v2.csv", []string{"id"})
+						if err != nil {
+							t.Error(err)
+						}
+						return newDf
+					}(),
+					[]string{"est_diameter_min", "miss_distance"},
+					[]GnuplotOpt{Using("0:1 lc 7")},
+				},
+			},
+			[]GnuplotOpt{},
+		},
+	}
+	for _, test := range plotTests {
+		err := PlotN(test.arg1, test.arg2...)
+		if err != nil {
+			t.Fatalf("error %v", err)
+		}
+	}
+}
