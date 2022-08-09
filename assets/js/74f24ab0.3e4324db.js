8cd3b8b703a1e59fdd8632ad417075cd32b57284
jpoly1219 <jpoly1219@users.noreply.github.com>
2022-08-09 02:02:43 +0000

    deploy: 583e48aa8dfe2cc0264bd268f8c5362a01ed015c

---
+++ assets/js/74f24ab0.3e4324db.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[4108],{3078:s=>{s.exports=JSON.parse('{"permalink":"/gambas/blog/tags/hola","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ assets/js/9b74e2c5.385e7094.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[8970],{478:s=>{s.exports=JSON.parse('{"label":"docusaurus","permalink":"/gambas/blog/tags/docusaurus","allTagsPath":"/gambas/blog/tags","count":4}')}}]);
