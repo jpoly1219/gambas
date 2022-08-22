134bc4c0caeb7e9264af1d59e4f6e5097c87c68a
Jacob Kim <jpoly1219@gmail.com>
2022-08-22 10:19:47 +0900

    updated bench input for SeriesQ3

--- series_test.go
+++ series_test.go
@@ -4958,13 +4958,13 @@ func BenchmarkSeriesQ3(b *testing.B) {
 		ser  Series
 	}{
 		{
-			"459 Points",
+			"1 Points",
 			func() Series {
-				testDf, err := ReadCsv("testfiles/nba.csv", []string{"Name"})
+				testDf, err := NewSampleDataFrame(map[string]string{"title": "string", "rating": "float64"}, 1, nil)
 				if err != nil {
 					b.Error(err)
 				}
-				testSer, err := testDf.LocCol("Salary")
+				testSer, err := testDf.LocCol("rating")
 				if err != nil {
 					b.Error(err)
 				}
@@ -4972,13 +4972,13 @@ func BenchmarkSeriesQ3(b *testing.B) {
 			}(),
 		},
 		{
-			"90837 Points",
+			"10 Points",
 			func() Series {
-				testDf, err := ReadCsv("testfiles/neo_v2.csv", []string{"id"})
+				testDf, err := NewSampleDataFrame(map[string]string{"title": "string", "rating": "float64"}, 10, nil)
 				if err != nil {
 					b.Error(err)
 				}
-				testSer, err := testDf.LocCol("est_diameter_min")
+				testSer, err := testDf.LocCol("rating")
 				if err != nil {
 					b.Error(err)
 				}
@@ -4986,13 +4986,83 @@ func BenchmarkSeriesQ3(b *testing.B) {
 			}(),
 		},
 		{
-			"4857378 Points",
+			"100 Points",
 			func() Series {
-				testDf, err := ReadCsv("testfiles/bitstampUSD_1-min_data_2012-01-01_to_2021-03-31.csv", []string{"Timestamp"})
+				testDf, err := NewSampleDataFrame(map[string]string{"title": "string", "rating": "float64"}, 100, nil)
 				if err != nil {
 					b.Error(err)
 				}
-				testSer, err := testDf.LocCol("Close")
+				testSer, err := testDf.LocCol("rating")
+				if err != nil {
+					b.Error(err)
+				}
+				return testSer
+			}(),
+		},
+		{
+			"1000 Points",
+			func() Series {
+				testDf, err := NewSampleDataFrame(map[string]string{"title": "string", "rating": "float64"}, 1000, nil)
+				if err != nil {
+					b.Error(err)
+				}
+				testSer, err := testDf.LocCol("rating")
+				if err != nil {
+					b.Error(err)
+				}
+				return testSer
+			}(),
+		},
+		{
+			"10000 Points",
+			func() Series {
+				testDf, err := NewSampleDataFrame(map[string]string{"title": "string", "rating": "float64"}, 10000, nil)
+				if err != nil {
+					b.Error(err)
+				}
+				testSer, err := testDf.LocCol("rating")
+				if err != nil {
+					b.Error(err)
+				}
+				return testSer
+			}(),
+		},
+		{
+			"100000 Points",
+			func() Series {
+				testDf, err := NewSampleDataFrame(map[string]string{"title": "string", "rating": "float64"}, 100000, nil)
+				if err != nil {
+					b.Error(err)
+				}
+				testSer, err := testDf.LocCol("rating")
+				if err != nil {
+					b.Error(err)
+				}
+				return testSer
+			}(),
+		},
+		{
+			"1000000 Points",
+			func() Series {
+				testDf, err := NewSampleDataFrame(map[string]string{"title": "string", "rating": "float64"}, 1000000, nil)
+				if err != nil {
+					b.Error(err)
+				}
+				testSer, err := testDf.LocCol("rating")
+				if err != nil {
+					b.Error(err)
+				}
+				return testSer
+			}(),
+		},
+		{
+			"10000000 Points",
+			func() Series {
+				testDf, err := NewSampleDataFrame(map[string]string{"title": "string", "rating": "float64"}, 10000000, nil)
+				if err != nil {
+					b.Error(err)
+				}
+				testSer, err := testDf.LocCol("rating")
 				if err != nil {
 					b.Error(err)
 				}
